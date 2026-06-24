3c5defa50a8774a39939b38090fc66a6c9cb9620
romainyu <romainyu@tencent.com>
2026-06-24 17:47:39 +0800

    feat(train): 支持通过 --no-use-amp 显式关闭 AMP 参数

--- experiments/DQM-DETR/train.py
+++ experiments/DQM-DETR/train.py
@@ -92,7 +92,8 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--tuning', type=str, help='tuning from checkpoint')
     parser.add_argument('-d', '--device', type=str, help='device',)
     parser.add_argument('--seed', type=int, help='exp reproducibility')
-    parser.add_argument('--use-amp', action='store_true', help='auto mixed precision training')
+    parser.add_argument('--use-amp', default=None, action=argparse.BooleanOptionalAction,
+                        help='auto mixed precision training (use --no-use-amp to force off)')
     parser.add_argument('--output-dir', type=str, help='output directoy')
     parser.add_argument('--summary-dir', type=str, help='tensorboard summry')
     parser.add_argument('--test-only', action='store_true', default=False,)
